e0660e9543836c709e906448e61b1c5c773bc689
jungle0127 <arbor@foxmail.com>
2017-06-30 16:45:23 +0800

    Added Jersey rest service, doesn't work now

--- RESTFull/src/main/java/com/ps/Swagger2.java
+++ RESTFull/src/main/java/com/ps/Swagger2.java
@@ -20,7 +20,7 @@ public class Swagger2 {
 		return new Docket(DocumentationType.SWAGGER_2)
 				.apiInfo(apiInfo())
 				.select()
-				.apis(RequestHandlerSelectors.basePackage("com.veritas"))
+				.apis(RequestHandlerSelectors.basePackage("com.ps"))
 				.paths(PathSelectors.any())
 				.build();
 			
